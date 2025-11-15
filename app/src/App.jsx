ccfffdf03d07459cc1b4b6d74bbbf869d5e9aecc
unknown <nandishdpatel22@gmail.com>
2025-11-15 10:49:04 -0600

    created boiler plate for react and tailwindcss

---
+++ app/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Hello just checking</h1>
+      <h2>Just for cheking purpose</h2>
+    </>
+  )
+}
+
+export default App;
